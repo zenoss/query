c6baa7924ff816fdc36eac3dd71c2fb12fe27835
David Bainbridge <davidk.bainbridge@gmail.com>
2013-07-11 17:49:18 -0700

    check point to have the the updates saved on more than a single system. this check point includes updates to the visualization JS library to dynamically load the required libraries an build a basic chart from static data.

--- src/main/resources/api/visualization.js
+++ src/main/resources/api/visualization.js
@@ -1,6 +1,250 @@
+var __data = {
+	"clientId" : "not-specified",
+	"source" : "OpenTSDB",
+	"startTime" : "10m-ago",
+	"startTimeActual" : "2013/07/11-22:50:33-+0000",
+	"endTime" : "now",
+	"endTimeActual" : "2013/07/11-23:00:33-+0000",
+	"exactTimeWindow" : true,
+	"series" : false,
+	"results" : [ {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583047,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583061,
+		"value" : 17.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583064,
+		"value" : 17.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583073,
+		"value" : 18.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583081,
+		"value" : 18.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583086,
+		"value" : 18.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583104,
+		"value" : 19.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583260,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583262,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583265,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583318,
+		"value" : 15.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583320,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583326,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583330,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583347,
+		"value" : 15.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583361,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583364,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583373,
+		"value" : 17.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583381,
+		"value" : 21.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583386,
+		"value" : 23.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583404,
+		"value" : 5.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583560,
+		"value" : 5.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583562,
+		"value" : 3.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583565,
+		"value" : 1.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583620,
+		"value" : 1.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583626,
+		"value" : 1.0
+	}, {
+		"metric" : "laLoadInt1",
+		"timestamp" : 1373583630,
+		"value" : 0.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583047,
+		"value" : 15.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583057,
+		"value" : 15.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583061,
+		"value" : 15.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583064,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583073,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583081,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583086,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583104,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583260,
+		"value" : 15.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583262,
+		"value" : 15.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583265,
+		"value" : 15.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583318,
+		"value" : 15.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583320,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583326,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583328,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583330,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583347,
+		"value" : 15.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583357,
+		"value" : 16.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583361,
+		"value" : 17.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583364,
+		"value" : 18.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583373,
+		"value" : 19.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583381,
+		"value" : 22.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583386,
+		"value" : 24.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583404,
+		"value" : 7.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583560,
+		"value" : 5.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583562,
+		"value" : 3.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583565,
+		"value" : 3.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583620,
+		"value" : 5.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583626,
+		"value" : 6.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583628,
+		"value" : 9.0
+	}, {
+		"metric" : "laLoadInt5",
+		"timestamp" : 1373583630,
+		"value" : 0.0
+	} ]
+};
+
 var zenoss = {
 	visualization : {
-		url : "http://localhost:80808",
+		url : "http://localhost:8080",
 		defaults : {
 			range : {
 				start : "1h-aog",
@@ -10,6 +254,68 @@ var zenoss = {
 
 			},
 			aggregation : "avg"
+		},
+
+		load : function(callback) {
+			function loadScript(url, async, callback) {
+				var script = document.createElement("script")
+				script.type = "text/javascript";
+				if (async) {
+					script.async = true;
+				}
+
+				if (script.readyState) { // IE
+					script.onreadystatechange = function() {
+						if (script.readyState == "loaded"
+								|| script.readyState == "complete") {
+							script.onreadystatechange = null;
+							callback();
+						}
+					};
+				} else { // Others
+					script.onload = function() {
+						callback();
+					};
+				}
+
+				script.src = url;
+				document.getElementsByTagName("head")[0].appendChild(script);
+			}
+
+			function includeCSS(url) {
+				var css = document.createElement('link');
+				css.rel = 'stylesheet'
+				css.type = 'text/css';
+				css.href = url;
+				document.getElementsByTagName('head')[0].appendChild(css);
+			}
+
+			// Script order matters and as this is all loaded asynchronously the
+			// loads are nested. Bootstrap load JQuery and then use JQuery to
+			// load everything else
+			loadScript(
+					'http://localhost:8888/api/jquery.min.js',
+					true,
+					function() {
+						console.log('JQuery loaded');
+						$
+								.getScript(
+										'http://localhost:8888/api/d3.v3.min.js',
+										function() {
+											console.log('D3 loaded');
+											$
+													.getScript(
+															'http://localhost:8888/api/nv.d3.min.js',
+															function() {
+																console
+																		.log('NV.D3 Loaded');
+																includeCSS('http://localhost:8888/api/css/nv.d3.css');
+																console
+																		.log("Loaded NV.D3 CSS");
+																callback();
+															});
+										});
+					});
 		},
 		timerange : {
 			create : function(name, startTime, endTime) {
@@ -117,11 +423,18 @@ var zenoss = {
 		},
 		chart : {
 			create : function(name, config) {
-				return new zenoss.visualization.Chart(name);
+				if (typeof jQuery == 'undefined') {
+					loadRequiredLibraries(function() {
+						return new zenoss.visualization.Chart(name, config);
+					});
+					return;
+				}
+				zenoss.visualization.Chart(name, config);
 			}
 		},
-		Chart : function(name) {
+		Chart : function(name, config) {
 			var _name = name;
+			var _config = config;
 			this.name = function() {
 				return _name;
 			}
@@ -131,6 +444,60 @@ var zenoss = {
 			this.add = function(name, options) {
 				return zenoss.visualization.plot.create(name, options);
 			}
+
+			if (typeof jQuery == 'undefined') {
+				console.log("no go");
+				return;
+			}
+
+			var div = $('#' + name);
+			if (typeof config.width != 'undefined') {
+				div.width(config.width);
+			}
+			if (typeof config.height != 'undefined') {
+				div.height(config.height);
+			}
+			// div.css('background-color', 'red');
+			var timeFormat = function(ts) {
+				return d3.time.format('%x %X')(new Date(ts));
+			}
+			var _d = [];
+			var plots = [];
+			__data.results.forEach(function(v) {
+				var plot = plots[v.metric];
+				if (typeof plot == 'undefined') {
+					plot = {
+						"key" : v.metric,
+						"values" : []
+					};
+					plots[v.metric] = plot;
+				}
+				plot.values.push({
+					"x" : v.timestamp * 1000,
+					"y" : v.value
+				});
+			});
+			// convert associative array plots to normal array
+			var _plots = [];
+
+			for ( var key in plots) {
+				if (plots.hasOwnProperty(key)) {
+					_plots.push(plots[key]);
+				}
+			}
+
+			var _chart = nv.models.lineChart();
+			var _svg = null;
+
+			nv.addGraph(function() {
+				_chart.xAxis.tickFormat(timeFormat).axisLabel('Date/Time');
+				_chart.yAxis.axisLabel('Memory');
+				_svg = d3.select('#' + _name).append('svg');
+				_svg.datum(_plots).transition().duration(500).call(_chart);
+				nv.utils.windowResize(function() {
+					_svg.call(_chart)
+				});
+			});
 		}
 	}
 }
