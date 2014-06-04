2fd063e5da2e674383f1b59c293c87b8d843b380
Mike Orr <morr@zenoss.com>
2014-06-04 14:34:30 -0500

    removing intellij boilerplate comments, adding Zenoss copyright headers

--- src/test/java/org/zenoss/app/metricservice/JerseyClientTest.java
+++ src/test/java/org/zenoss/app/metricservice/JerseyClientTest.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2014, Zenoss and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Zenoss or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.zenoss.app.metricservice;
 
 import com.sun.jersey.api.client.Client;
@@ -11,13 +42,6 @@ import org.zenoss.app.metricservice.api.model.MetricSpecification;
 import javax.ws.rs.core.MediaType;
 import java.util.*;
 
-/**
- * Created with IntelliJ IDEA.
- * User: morr
- * Date: 5/6/14
- * Time: 3:04 PM
- * To change this template use File | Settings | File Templates.
- */
 public class JerseyClientTest {
     @Test
     @Ignore("not really a unit test")
