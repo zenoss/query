0f1a1d778cefacd4dca3e64ff4ebb63df58564c9
John Matos <rDr4g0n@gmail.com>
2014-06-05 15:03:44 -0500

    increased memory available to central query

--- src/main/bin/central-query.sh
+++ src/main/bin/central-query.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 JVM_ARGS="$JVM_ARGS"
-JVM_XMX="-Xmx1024m"
+JVM_XMX="-Xmx4096m"
+JVM_XMS="-Xms1024m"
 
 LIBDIR=lib/${project.artifactId}
 ZAPP_JAR=${LIBDIR}/${project.artifactId}-${project.version}.jar
 
 if [ -f ${ZAPP_JAR} ]; then
-    exec java -server -XX:+HeapDumpOnOutOfMemoryError ${JVM_XMX} ${JVM_ARGS}  -jar ${ZAPP_JAR} server etc/${project.artifactId}/configuration.yaml
+    exec java -server -XX:+HeapDumpOnOutOfMemoryError ${JVM_XMX} ${JVM_XMS} ${JVM_ARGS}  -jar ${ZAPP_JAR} server etc/${project.artifactId}/configuration.yaml
 else
     cd ${LIBDIR} &&  mvn -DskipTests=true compile exec:java
 fi
