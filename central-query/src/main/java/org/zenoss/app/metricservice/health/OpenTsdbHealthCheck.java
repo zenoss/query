2960f4ec8d9cdf9a575da18c35a97d5594a25c40
Rye Terrell <rterrell@zenoss.com>
2016-01-04 14:26:05 -0600

    minor syntax improvement

--- central-query/src/main/java/org/zenoss/app/metricservice/health/OpenTsdbHealthCheck.java
+++ central-query/src/main/java/org/zenoss/app/metricservice/health/OpenTsdbHealthCheck.java
@@ -31,7 +31,8 @@
 package org.zenoss.app.metricservice.health;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.zenoss.app.metricservice.MetricServiceAppConfiguration;
