1054c210690364610cec77de6e7da1db5b406652
rderkach <rderk@softserveinc.com>
2016-07-19 09:07:32 +0300

    ZEN-23521: removed heap dump option on OOM

--- central-query/src/main/bin/central-query.sh
+++ central-query/src/main/bin/central-query.sh
@@ -8,7 +8,7 @@ LIBDIR=lib/${project.artifactId}
 ZAPP_JAR=${LIBDIR}/${project.artifactId}-${project.version}.jar
 
 if [ -f ${ZAPP_JAR} ]; then
-    exec java -server -XX:+HeapDumpOnOutOfMemoryError -XX:OnOutOfMemoryError="kill -9 %p" ${JVM_XMX} ${JVM_XMS} ${JVM_ARGS}  -jar ${ZAPP_JAR} server etc/${project.artifactId}/configuration.yaml
+    exec java -server -XX:OnOutOfMemoryError="kill -9 %p" ${JVM_XMX} ${JVM_XMS} ${JVM_ARGS}  -jar ${ZAPP_JAR} server etc/${project.artifactId}/configuration.yaml
 else
     cd ${LIBDIR} &&  mvn -DskipTests=true compile exec:java -pl central-query
 fi
