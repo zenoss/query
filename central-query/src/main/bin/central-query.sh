c14735551bf96c84daeadb9e15f33e8db6c5ea75
John Crocker <jcrocker@zenoss.com>
2017-02-27 10:41:13 -0600

    Modify script to allow dev debugger attachment

--- central-query/src/main/bin/central-query.sh
+++ central-query/src/main/bin/central-query.sh
@@ -11,5 +11,9 @@ if [ -f ${ZAPP_JAR} ]; then
     exec java -server -XX:OnOutOfMemoryError="kill -9 %p" ${JVM_XMX} ${JVM_XMS} ${JVM_ARGS}  -jar ${ZAPP_JAR} server etc/${project.artifactId}/configuration.yaml
 else
     # compile and install projects before running
-    cd ${LIBDIR} && mvn -DskipTests=true compile install && mvn -DskipTests=true exec:java -pl ${project.artifactId}
+    cd ${LIBDIR} && \
+        mvn -DskipTests=true compile install && \
+        exec java -server -XX:+HeapDumpOnOutOfMemoryError ${JVM_XMX} ${JVM_XMS} ${JVM_ARGS} \
+            -jar ${project.artifactId}/target/${project.artifactId}-${project.version}.jar \
+            server ${project.artifactId}/src/main/etc/configuration.yaml
 fi
