1c6f4f11c773725739488dd47b270bb3f65233d1
Mike Orr <morr@zenoss.com>
2017-08-22 17:02:18 -0500

    fix unit test

--- central-query-lib/src/main/java/org/zenoss/app/metricservice/api/impl/QueryResult.java
+++ central-query-lib/src/main/java/org/zenoss/app/metricservice/api/impl/QueryResult.java
@@ -34,6 +34,8 @@ package org.zenoss.app.metricservice.api.impl;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 import java.util.*;
 
@@ -100,5 +102,33 @@ public class QueryResult {
 
     public Map<String, Collection<String>> getTags() {
         return tags.asMap();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        QueryResult that = (QueryResult) o;
+
+        return new EqualsBuilder()
+                .append(datapoints, that.datapoints)
+                .append(metric, that.metric)
+                .append(tags, that.tags)
+                .append(queryStatus, that.queryStatus)
+                .append(id, that.id)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(datapoints)
+                .append(metric)
+                .append(tags)
+                .append(queryStatus)
+                .append(id)
+                .toHashCode();
     }
 }
